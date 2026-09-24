eea8cb3e3fa84054cb8882493913256a05a8ae85
辛磊 <xinleibird@gmail.com>
2026-09-25 05:00:36 +0800

    chore: add stylua config (2-space) and reformat VRMInterrupt.lua

--- VRMInterrupt.lua
+++ VRMInterrupt.lua
@@ -1,34 +1,34 @@
 -- VRMInterrupt.lua
 
 function VRMINTCast()
-	if not UnitExists("target") then
-		return
-	end
+  if not UnitExists("target") then
+    return
+  end
 
-	if not (C_Spell and C_Spell.UnitCastingInfo) then
-		return
-	end
+  if not (C_Spell and C_Spell.UnitCastingInfo) then
+    return
+  end
 
-	local castName, notInterruptible
+  local castName, notInterruptible
 
-	local name, _, _, _, _, _, _, castNotInterruptible = C_Spell.UnitCastingInfo("target")
-	if name then
-		castName, notInterruptible = name, castNotInterruptible
-	else
-		local cname, _, _, _, _, _, channelNotInterruptible = C_Spell.UnitChannelInfo("target")
-		if cname then
-			castName, notInterruptible = cname, channelNotInterruptible
-		end
-	end
+  local name, _, _, _, _, _, _, castNotInterruptible = C_Spell.UnitCastingInfo("target")
+  if name then
+    castName, notInterruptible = name, castNotInterruptible
+  else
+    local cname, _, _, _, _, _, channelNotInterruptible = C_Spell.UnitChannelInfo("target")
+    if cname then
+      castName, notInterruptible = cname, channelNotInterruptible
+    end
+  end
 
-	if not castName then
-		return
-	end
+  if not castName then
+    return
+  end
 
-	if notInterruptible then
-		DEFAULT_CHAT_FRAME:AddMessage(ConsoleColor .. "目标法术不可打断。|r")
-		return
-	end
+  if notInterruptible then
+    DEFAULT_CHAT_FRAME:AddMessage(ConsoleColor .. "目标法术不可打断。|r")
+    return
+  end
 
-	CastSpellByName("脚踢")
+  CastSpellByName("脚踢")
 end
